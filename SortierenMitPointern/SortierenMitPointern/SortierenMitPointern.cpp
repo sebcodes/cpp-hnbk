10446e31a3374407e7bff4b38c60ae56ced08a44
sebcodes <info@sebastian-kiefer.com>
2020-11-07 23:02:14 +0100

    Sortierung von Zahlen mit Pointern

--- SortierenMitPointern/SortierenMitPointern/SortierenMitPointern.cpp
+++ SortierenMitPointern/SortierenMitPointern/SortierenMitPointern.cpp
@@ -1,5 +1,10 @@
-// Sortieren.cpp : Definiert den Einstiegspunit für die Konsolenanwendung.
-//
+/*
+* Application "Alle Projekte (Aufgaben) die wir in Info gemacht haben"
+* Copyright (c) sebcodes 2020
+* @author TimPreisler <https://github.com/TimIsNotAnymoreLost>
+* @author sebcodes <info@sebcodes.de>
+* @see https://github.com/sebcodes/cpp-hnbk
+*/
 
 #include <iostream>
 using namespace std;
